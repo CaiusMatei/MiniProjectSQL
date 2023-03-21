fcf749663da27df9d4037e01e80586f26a804584
Caius Matei <caius.matei@chasacademy.se>
2023-03-21 18:09:59 +0100

    first commit

---
+++ database.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE "public"."cai_project" ( 
+  "id" SERIAL,
+  "project_name" VARCHAR(35) NOT NULL,
+  CONSTRAINT "cai_project_pkey" PRIMARY KEY ("id"),
+  CONSTRAINT "cai_project_name" UNIQUE ("project_name")
+);
+CREATE TABLE "public"."cai_project_person" ( 
+  "id" SERIAL,
+  "project_id" INTEGER NOT NULL,
+  "person_id" INTEGER NOT NULL,
+  "hours" INTEGER NOT NULL,
+  CONSTRAINT "cai_project_person_pkey" PRIMARY KEY ("id")
+);
+CREATE TABLE "public"."cai_person" ( 
+  "id" SERIAL,
+  "person_name" VARCHAR(20) NOT NULL,
+  CONSTRAINT "cai_person_pkey" PRIMARY KEY ("id"),
+  CONSTRAINT "cai_person_name" UNIQUE ("person_name")
+);
+ALTER TABLE "public"."cai_project_person" ADD CONSTRAINT "FK_cai_project_person_project_id" FOREIGN KEY ("project_id") REFERENCES "public"."cai_project" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "public"."cai_project_person" ADD CONSTRAINT "FK_cai_person_project_person_id" FOREIGN KEY ("person_id") REFERENCES "public"."cai_person" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
